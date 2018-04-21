5eb302c2f809c9bdc030c7abdbecb66f42bdb3e5
Eduardo Alvarez <eduardo.alvarez@globant.com>
2018-04-21 02:12:21 -0300

    Ready for development

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class Main extends Component {
-  render () {
-    return(<h1>The Best Music Player</h1>)
-  };
-};
+const Main = () => (<h1>The Best Music Player</h1>);
 
 ReactDOM.render(<Main />, document.getElementById('app'));
